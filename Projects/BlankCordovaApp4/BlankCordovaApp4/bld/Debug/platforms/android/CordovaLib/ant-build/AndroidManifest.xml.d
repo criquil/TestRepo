6e15573fb92871ba05579ac562dfeab92df93169
DIR\cristian.rosa <cristian.rosa@accenture.com>
2014-10-04 17:21:31 -0300

    First Commit

---
+++ Projects/BlankCordovaApp4/BlankCordovaApp4/bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\test\Projects\BlankCordovaApp4\BlankCordovaApp4\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\test\Projects\BlankCordovaApp4\BlankCordovaApp4\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ Projects/BlankCordovaApp4/BlankCordovaApp4/bld/Debug/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\test\Projects\BlankCordovaApp4\BlankCordovaApp4\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
